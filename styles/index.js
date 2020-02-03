02827521f697f8a4c22a47a3e7330156522d2874
Tom <tom@bearjam.dev>
2020-02-03 13:40:09 +0000

    min-height > height

--- styles/index.js
+++ styles/index.js
@@ -8,7 +8,7 @@ export const globalStyles = css`
     padding: 0;
     margin: 0;
     background: white;
-    min-height: 100%;
+    height: 100%;
     font-family: Helvetica, Arial, sans-serif;
   }
 `
